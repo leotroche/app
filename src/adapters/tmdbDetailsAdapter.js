1cfcde06e1cae8d932472084bc9e605f6637a074
Leonardo Troche <leo.m.troche@gmail.com>
2023-04-04 11:46:18 -0300

    refactor: :recycle: refactor for adapters

--- src/adapters/tmdbDetailsAdapter.js
+++ src/adapters/tmdbDetailsAdapter.js
@@ -1,28 +1,42 @@
-import { TMDB } from '../api/axiosInstance'
+import { BASE_URL } from '../config/tmdb'
+import { convertMinutesToHours } from '../utils/convertMinutesToHours'
+import { formatRating } from '../utils/formatRating'
 
 export function tmdbDetailsAdapter(item) {
   const isMovie = !!item.title
 
-  const details = {
-    backdrop: TMDB.path.images.backdrop + item.backdrop_path,
+  const backdrop = BASE_URL.backdrop + item.backdrop_path
+  const poster = BASE_URL.poster + item.poster_path
+  const rating = formatRating(item?.vote_average)
+  const runtime = convertMinutesToHours(item?.runtime)
+
+  const data = {
+    backdrop,
     description: item.overview,
-    genres: item.genres.map((genre) => genre.name),
+    genres: item.genres,
     homepage: item.homepage,
     id: item.id,
-    isMovie,
-    poster: TMDB.path.images.poster + item.poster_path,
-    rating: item.vote_average,
-    releaseDate: isMovie ? item.release_date : item.first_air_date,
-    title: isMovie ? item.title : item.name,
+    poster,
+    rating,
+    status: item.status,
   }
 
   if (isMovie) {
-    return { ...details, runtime: item.runtime }
+    return {
+      ...data,
+      releaseDate: item.release_date,
+      runtime,
+      title: item.title,
+      type: 'movie',
+    }
   }
 
   return {
-    ...details,
+    ...data,
     numberOfEpisodes: item.number_of_episodes,
     numberOfSeasons: item.number_of_seasons,
+    releaseDate: item.first_air_date,
+    title: item.name,
+    type: 'tv',
   }
 }
--- src/adapters/tmdbListAdapter.js
+++ src/adapters/tmdbListAdapter.js
@@ -1,18 +1,36 @@
-import { TMDB } from '../api/axiosInstance'
+import { BASE_URL } from '../config/tmdb'
+import { formatRating } from '../utils/formatRating'
 
 export function tmdbListAdapter(items) {
   return items.map((item) => {
     const isMovie = !!item.title
 
-    return {
-      backdrop: TMDB.path.images.backdrop + item.backdrop_path,
+    const backdrop = BASE_URL.backdrop + item.backdrop_path
+    const poster = BASE_URL.poster + item.poster_path
+    const rating = formatRating(item?.vote_average)
+
+    const data = {
+      backdrop,
       description: item.overview,
       id: item.id,
-      isMovie,
-      poster: TMDB.path.images.poster + item.poster_path,
-      rating: item.vote_average,
-      releaseDate: isMovie ? item.release_date : item.first_air_date,
-      title: isMovie ? item.title : item.name,
+      poster,
+      rating,
+    }
+
+    if (isMovie) {
+      return {
+        ...data,
+        releaseDate: item.release_date,
+        title: item.title,
+        type: 'movie',
+      }
+    }
+
+    return {
+      ...data,
+      releaseDate: item.first_air_date,
+      title: item.name,
+      type: 'tv',
     }
   })
 }
